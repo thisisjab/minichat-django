ea70d592c82e677556501c929b8e954df1247fa1
thisisjab <heythisisjab@gmail.com>
2024-01-11 00:17:56 +0330

    feat: configure postgres database

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("PROJECT_POSTGRES_DB"),
+        "USER": env("PROJECT_POSTGRES_USER"),
+        "PASSWORD": env("PROJECT_POSTGRES_PASSWORD"),
+        "HOST": env("PROJECT_POSTGRES_HOST", default="db"),
+        "PORT": env("PROJECT_POSTGRES_PORT", default="5432"),
     }
 }
 
