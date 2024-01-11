5103b085e84842324114e2f1e8c6a5def142b268
thisisjab <heythisisjab@gmail.com>
2024-01-11 23:47:37 +0330

    chore(settings): configure templates and static files path

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -101,7 +101,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "core" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -166,7 +166,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "core/static/"
+STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
