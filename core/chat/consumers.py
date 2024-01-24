3bb2819bb10da8c2b928cb59833b839118ffd145
thisisjab <heythisisjab@gmail.com>
2024-01-24 23:56:19 +0330

    feat(chat): handle data recieve in chat consumer

--- core/chat/consumers.py
+++ core/chat/consumers.py
@@ -15,3 +15,16 @@ class ChatConsumer(WebsocketConsumer):
                 }
             )
         )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        self.send(
+            text_data=json.dumps(
+                {
+                    "type": "chat",
+                    "message": f"{self.scope['user'].username}: {message}",
+                }
+            )
+        )
