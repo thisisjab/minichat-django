3dc14ed3678b476d8855e4e80ec3127bfc171667
thisisjab <heythisisjab@gmail.com>
2024-01-25 00:07:56 +0330

    feat(chat): create event handlers for chat

--- core/chat/consumers.py
+++ core/chat/consumers.py
@@ -1,30 +1,39 @@
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        self.room_group_name = "test"
 
-        self.send(
-            text_data=json.dumps(
-                {
-                    "type": "connection_established",
-                    "message": "You are now connected to the chat server.",
-                }
-            )
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
         )
+        self.accept()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": f"{self.scope['user'].username}: {message}",
+            },
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
+
         self.send(
             text_data=json.dumps(
                 {
                     "type": "chat",
-                    "message": f"{self.scope['user'].username}: {message}",
+                    "message": f"{message}",
                 }
             )
         )
