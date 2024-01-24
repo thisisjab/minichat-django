8c2ddcfb966f2c87c8a64624b4a88cbdea8f51fc
thisisjab <heythisisjab@gmail.com>
2024-01-24 23:59:15 +0330

    chore(chat): enable in memory channel layers

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -123,7 +123,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 ASGI_APPLICATION = "core.asgi.application"
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
