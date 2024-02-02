2e466ebdae186f34d252bc74bd81de7ac965c8f0
thisisjab <heythisisjab@gmail.com>
2024-02-02 17:44:55 +0330

    feat(chat): complete chat box to show messages

--- core/chat/views.py
+++ core/chat/views.py
@@ -1,7 +1,8 @@
 from typing import Self
+from uuid import UUID
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
@@ -40,6 +41,66 @@ class ChatListView(LoginRequiredMixin, View):
         )
 
 
-class ConversationView(View):
-    def get(self, request, id):
-        return render(request, "chat/conversation.html", context={"room_name": id})
+class ConversationView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request: HttpRequest, id: UUID):
+        user_active_participations = models.Participation.objects.filter(
+            user=request.user, is_active=True
+        ).all()
+
+        user_active_chats = []
+
+        for participation in user_active_participations:
+            chat_name = (
+                participation.chat.participations.exclude(user=request.user)
+                .last()
+                .user.username
+            )
+
+            chat = {
+                "id": participation.chat.id,
+                "type": participation.chat.type,
+                "last_modified_at": participation.chat.last_modified_at(),
+                "name": chat_name,
+            }
+
+            user_active_chats.append(chat)
+
+        chat = (
+            models.Chat.objects.prefetch_related("messages")
+            .filter(id=self.kwargs["id"])
+            .last()
+        )
+
+        chat_messages = []
+
+        for message in chat.messages.all():
+            chat_message = {
+                "id": message.id,
+                "body": message.body,
+                "modified_at": message.modified_at,
+                "is_sent": True if message.sender == request.user else False,
+            }
+
+            chat_messages.append(chat_message)
+
+        return render(
+            request,
+            "chat/conversation.html",
+            context={
+                "user_active_chats": user_active_chats,
+                "chat_messages": chat_messages,
+            },
+        )
+
+    def test_func(self: Self):
+        """Check if chat exists and user is a participant of it."""
+        chat = models.Chat.objects.filter(id=self.kwargs["id"])
+        if not chat.exists():
+            return False
+
+        if not self.request.user.participations.filter(
+            chat__id=self.kwargs["id"], is_active=True
+        ).exists():
+            return False
+
+        return True
