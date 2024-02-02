a244c2715d889b0161d88593a81edf691c769d0f
thisisjab <heythisisjab@gmail.com>
2024-02-02 12:54:44 +0330

    feat(chat): register models in admin

--- core/chat/admin.py
+++ core/chat/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from core.chat import models
+
+# TODO: complete model admins
+
+
+@admin.register(models.Participation)
+class ParticipationAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Chat)
+class ChatAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.TextMessage)
+class TextMessageAdmin(admin.ModelAdmin):
+    ...
