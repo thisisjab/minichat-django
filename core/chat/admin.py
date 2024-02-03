750369a33e63301d8c564b2a5b9f777a989ec03e
thisisjab <heythisisjab@gmail.com>
2024-02-03 21:40:33 +0330

    !feat(chat): create `BasePrivateMessage` and `PrivateTextMessage` models and their admin

--- core/chat/admin.py
+++ core/chat/admin.py
@@ -1,20 +1,77 @@
+from typing import Self
+
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+from django.urls import reverse
+from django.utils.html import format_html
 
 from core.chat import models
 
-# TODO: complete model admins
 
+@admin.register(models.PrivateTextMessage)
+class TextPrivateMessageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "message_body",
+        "sender_username",
+        "receiver_username",
+        "modified_at",
+        "created_at",
+    ]
 
-@admin.register(models.Participation)
-class ParticipationAdmin(admin.ModelAdmin):
-    ...
+    list_filter = [
+        "modified_at",
+        "created_at",
+    ]
 
+    list_per_page = 20
 
-@admin.register(models.Chat)
-class ChatAdmin(admin.ModelAdmin):
-    ...
+    ordering = [
+        "modified_at",
+        "created_at",
+    ]
 
+    raw_id_fields = [
+        "sender",
+        "receiver",
+    ]
 
-@admin.register(models.TextMessage)
-class TextMessageAdmin(admin.ModelAdmin):
-    ...
+    readonly_fields = [
+        "created_at",
+        "modified_at",
+    ]
+
+    search_fields = [
+        "body",
+        "sender",
+        "receiver",
+    ]
+
+    search_help_text = "Search by message body and sender/receiver name."
+
+    def message_body(self: Self, message: models.PrivateTextMessage):
+        return truncatechars(message.body, 50)
+
+    @admin.display(ordering="sender__username")
+    def sender_username(self: Self, message: models.PrivateTextMessage):
+        link = reverse(
+            "admin:users_user_change",
+            args=[message.sender.pk],
+        )
+        return format_html(
+            '<a href="{}">{}</a>',
+            link,
+            message.sender.username,
+        )
+
+    @admin.display(ordering="receiver__username")
+    def receiver_username(self: Self, message: models.PrivateTextMessage):
+        link = reverse(
+            "admin:users_user_change",
+            args=[message.receiver.pk],
+        )
+        return format_html(
+            '<a href="{}">{}</a>',
+            link,
+            message.receiver.username,
+        )
--- core/chat/models.py
+++ core/chat/models.py
@@ -3,7 +3,9 @@ from typing import Self
 
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator
 from django.db import models
+from django.template.defaultfilters import truncatechars
 from django.utils.translation import gettext_lazy as _
 
 from core.utils.models import TimeStampedModel
@@ -11,118 +13,59 @@ from core.utils.models import TimeStampedModel
 User = get_user_model()
 
 
-class Participation(models.Model):
-    """Model that represents a many-to-many relation between a chat and user."""
-
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="participations",
-        verbose_name=_("User"),
-    )
-    chat = models.ForeignKey(
-        "Chat",
-        on_delete=models.CASCADE,
-        related_name="participations",
-    )
-    created_at = models.DateTimeField(
-        _("Created at"),
-        auto_now_add=True,
-    )
-    is_active = models.BooleanField(
-        default=True,
-        null=False,
-        blank=False,
-        verbose_name=_("Is active"),
-    )
-
-
-class Chat(models.Model):
-    """Model that represents a chat between two or more users.
-
-    Raises:
-        ValidationError: Validation error raised more that two users are added as participants.
-    """
-
-    class Type(models.TextChoices):
-        PRIVATE = "P", _("Private Chat")
-        GROUP = "G", _("Group Chat")
+class BasePrivateMessage(TimeStampedModel):
+    """Base class for all type of private messages (text, image, etc.)"""
 
     id = models.UUIDField(
+        _("Id"),
         primary_key=True,
-        blank=False,
-        null=False,
         default=uuid.uuid4,
         editable=False,
-        verbose_name=_("ID"),
-    )
-
-    participants = models.ManyToManyField(
-        User,
-        related_name="chats",
-        through=Participation,
-        verbose_name=_("Participants"),
-    )
-
-    type = models.CharField(
-        max_length=1,
-        choices=Type.choices,
-        blank=False,
         null=False,
-    )
-
-    created_at = models.DateTimeField(
-        _("Created at"),
-        auto_now_add=True,
-    )
-
-    def clean(self: Self) -> None:
-        # In private conversations only two users can participate at most
-        if self.type == self.Type.PRIVATE and self.participants.count() > 2:
-            raise ValidationError(_("Only two users can be added to one private chat."))
-
-        return super().clean()
-
-    def __str__(self: Self) -> str:
-        # TODO: complete this function
-        return super().__str__()
-
-    def last_modified_at(self: Self):
-        if self.messages.annotate():
-            return self.messages.last().modified_at
-
-        return None
-
-
-class BaseMessage(TimeStampedModel):
-    """Base model for any message sent in a chat."""
-
-    id = models.UUIDField(
-        primary_key=True,
         blank=False,
-        null=False,
-        default=uuid.uuid4,
-        editable=False,
-        verbose_name=_("ID"),
-    )
-
-    chat = models.ForeignKey(
-        Chat,
-        on_delete=models.CASCADE,
-        related_name="messages",
-        verbose_name=_("Chat"),
     )
 
     sender = models.ForeignKey(
         User,
         on_delete=models.PROTECT,
-        related_name="sent_messages",
+        related_name="sent_private_messages",
+        blank=False,
+        null=False,
         verbose_name=_("Sender"),
+    )
+
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name="received_private_messages",
+        blank=False,
+        null=False,
+        verbose_name=_("Receiver"),
     )
 
     class Meta:
         abstract = True
 
+    def clean(self: Self) -> None:
+        if self.sender == self.receiver:
+            raise ValidationError(_("Sender and receiver cannot be the same."))
 
-class TextMessage(BaseMessage):
-    body = models.CharField(max_length=255, verbose_name=_("Body"))
+        return super().clean()
+
+
+class PrivateTextMessage(BasePrivateMessage):
+    """This class represents a basic text message sent in a private chat."""
+
+    body = models.TextField(
+        null=False,
+        blank=False,
+        validators=[MaxLengthValidator(4096)],
+        verbose_name=_("Body"),
+    )
+
+    class Meta:
+        verbose_name = "Private text message"
+        verbose_name_plural = "Private text messages"
+
+    def __str__(self: Self):
+        return truncatechars(self.body, 50)
