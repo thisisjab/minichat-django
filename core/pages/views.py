b78c44722972c86fb1ccf347cbbc7f4e3cbb5c20
thisisjab <heythisisjab@gmail.com>
2024-02-04 14:30:27 +0330

    feat: redirect user to chat list view if authenticated

--- core/pages/views.py
+++ core/pages/views.py
@@ -1,5 +1,17 @@
+from typing import Any
+
+from django.http import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "pages/index.html"
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            return redirect(reverse("chat:list"))
+
+        return super().get(request, *args, **kwargs)
