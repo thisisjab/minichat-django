cdbe449f03a609e413d1613548e69b8bfb84f8cd
thisisjab <heythisisjab@gmail.com>
2024-02-04 15:30:07 +0330

    !chore(chat): remove all views and urls

--- core/chat/urls.py
+++ core/chat/urls.py
@@ -1,10 +1,3 @@
-from django.urls import path
-
-from core.chat import views
-
 app_name = "chat"
 
-urlpatterns = [
-    path("", views.ChatListView.as_view(), name="list"),
-    path("<uuid:id>/", views.ConversationView.as_view(), name="conversation"),
-]
+urlpatterns = []
