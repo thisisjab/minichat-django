e8241162f47a064cea9a9646e78bb88fd4e0a244
thisisjab <heythisisjab@gmail.com>
2024-02-06 13:12:19 +0330

    refactor(chat): remove unused permissions

--- core/chat/consumers.py
+++ core/chat/consumers.py
@@ -5,23 +5,11 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 from core.chat import models
-from core.chat.permissions import check_user_is_a_participant
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self: Self):
-        self.chat_id = self.scope["path"].split("/")[3]
-        user = self.scope["user"]
-        if not check_user_is_a_participant(user, self.chat_id):
-            self.disconnect()
-
-        self.room_group_name = self.chat_id
-
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name,
-        )
-        self.accept()
+        ...
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
