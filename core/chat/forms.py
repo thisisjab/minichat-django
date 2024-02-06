8f2f6865f39a542836819db39cacfb4965e436b0
thisisjab <heythisisjab@gmail.com>
2024-02-06 22:00:01 +0330

    refactor(chat): move logic for checking existing private chats with given users to `utils`

--- core/chat/forms.py
+++ core/chat/forms.py
@@ -2,7 +2,7 @@ from typing import Any, Self
 
 from django.forms import ModelForm, ValidationError
 
-from core.chat import models
+from core.chat import models, utils
 
 
 class PrivateChatAddForm(ModelForm):
@@ -16,16 +16,7 @@ class PrivateChatAddForm(ModelForm):
         if participants and participants.count() != 2:
             raise ValidationError("A private chat must have 2 and only 2 participants.")
 
-        existing_private_chat = models.PrivateChat.objects.filter(
-            participants=participants[0]
-        )
-
-        for participant in participants[1:]:
-            existing_private_chat = existing_private_chat.filter(
-                participants=participant
-            )
-
-        if existing_private_chat.exists():
+        if utils.check_if_chat_exists_for_participants(participants):
             raise ValidationError("Another private chat exists for selected users.")
 
         return super().clean()
--- core/chat/utils.py
+++ core/chat/utils.py
@@ -1,4 +1,8 @@
+from typing import List
+
 from django.contrib.auth import get_user_model
+
+from core.chat import models
 
 User = get_user_model()
 
@@ -21,3 +25,25 @@ def get_last_messages_for_user(user: User):
             )
 
     return last_messages
+
+
+def check_if_chat_exists_for_participants(participants: List[User]) -> bool:
+    """Checks if a chat with given participants exists.
+
+    Args:
+        participants (List[User]): a list of `User` objects
+
+    Returns:
+        bool: True if there is a chat with given participants
+    """
+    existing_private_chat = models.PrivateChat.objects.filter(
+        participants=participants[0]
+    )
+
+    for participant in participants[1:]:
+        existing_private_chat = existing_private_chat.filter(participants=participant)
+
+    if existing_private_chat.exists():
+        return True
+
+    return False
