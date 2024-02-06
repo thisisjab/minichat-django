c27f010c5cc98c4fcffc57c4b26b28aee9cbf85a
thisisjab <heythisisjab@gmail.com>
2024-02-06 19:05:28 +0330

    feat(chat): add `PrivateChat` model

--- core/chat/models.py
+++ core/chat/models.py
@@ -13,6 +13,40 @@ from core.utils.models import TimeStampedModel
 User = get_user_model()
 
 
+class PrivateChat(models.Model):
+    """This model represents a private chat between two individuals.
+
+    NOTE: As m2m fields cannot be validated in model level, always use forms or
+    serializers to validate that 2 and only 2 participants exist. Also, before
+    creating new chat instance make sure that chat instance does not exist.
+    """
+
+    id = models.UUIDField(
+        _("Id"),
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        null=False,
+        blank=False,
+    )
+
+    participants = models.ManyToManyField(
+        User,
+        related_name="private_chats",
+        verbose_name=_("Participants"),
+    )
+
+    class Meta:
+        verbose_name = "private chat"
+        verbose_name_plural = "private chats"
+
+    def __str__(self: Self) -> str:
+        # NOTE: remember to pre_fetch `participants` field
+        return ", ".join(
+            [user.username for user in self.participants.order_by("username").all()]
+        )
+
+
 class BasePrivateMessage(TimeStampedModel):
     """Base class for all type of private messages (text, image, etc.)"""
 
@@ -34,21 +68,22 @@ class BasePrivateMessage(TimeStampedModel):
         verbose_name=_("Sender"),
     )
 
-    receiver = models.ForeignKey(
-        User,
-        on_delete=models.PROTECT,
-        related_name="received_private_messages",
-        blank=False,
-        null=False,
-        verbose_name=_("Receiver"),
+    related_chat = models.ForeignKey(
+        PrivateChat,
+        on_delete=models.CASCADE,
+        related_name="messages",
+        verbose_name=_("Related chat"),
     )
 
     class Meta:
         abstract = True
 
     def clean(self: Self) -> None:
-        if self.sender == self.receiver:
-            raise ValidationError(_("Sender and receiver cannot be the same."))
+        if self.sender not in self.related_chat.participants.all():
+            raise ValidationError(
+                "Cannot send a message since sender is not "
+                "a participant of the related chat."
+            )
 
         return super().clean()
 
