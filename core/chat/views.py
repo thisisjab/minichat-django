b6ddcba291f4b18140a00236d4470d805df80d0d
thisisjab <heythisisjab@gmail.com>
2024-02-06 22:15:05 +0330

    refactor(chat): change `ConversationView` regarding to `PrivateChat` model

--- core/chat/views.py
+++ core/chat/views.py
@@ -2,12 +2,12 @@ from typing import Any, Self
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 from core.chat import models
-from core.chat.utils import get_last_messages_for_user
+from core.chat.utils import (check_if_chat_exists_for_participants,
+                             get_last_messages_for_user)
 
 User = get_user_model()
 
@@ -32,19 +32,23 @@ class ConversationView(LoginRequiredMixin, TemplateView):
     def get_context_data(self: Self, **kwargs: Any) -> dict[str, Any]:
         user = self.request.user
         peer_user = get_object_or_404(User, username=kwargs.get("username", None))
-
-        chat_messages = (
-            models.PrivateTextMessage.objects.filter(
-                Q(sender=user, receiver=peer_user) | Q(sender=peer_user, receiver=user)
-            )
-            .order_by("modified_at")
-            .all()
-        )
-
         context = super().get_context_data(**kwargs)
 
-        last_messages = get_last_messages_for_user(user)
+        if not check_if_chat_exists_for_participants([user, peer_user]):
+            private_chat = models.PrivateChat.objects.create()
+            private_chat.participants.add(user)
+            private_chat.participants.add(peer_user)
+            context["chat_messages"] = list()
+            return context
 
+        last_messages = get_last_messages_for_user(user)
+        chat_messages = (
+            models.PrivateChat.objects.filter(participants=user)
+            .filter(participants=peer_user)
+            .first()
+            .messages.order_by("modified_at")
+            .all()
+        )
         context["last_messages"] = last_messages
         context["chat_messages"] = chat_messages
 
