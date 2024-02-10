7ce3a2716ba3aeb0e9e06fc11cc17d02f54adaae
thisisjab <heythisisjab@gmail.com>
2024-02-10 16:06:31 +0330

    feat(chat): create a new chat consumer that add user to all groups

--- core/chat/consumers.py
+++ core/chat/consumers.py
@@ -10,68 +10,103 @@ from core.chat import models, schemas, utils
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S %z"
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class WSConsumer(AsyncWebsocketConsumer):
     async def connect(self: Self):
         user = self.scope["user"]
-        chat_id = self.scope["path"].split("/")[3]
 
         if not user.is_authenticated:
             await self.disconnect()
 
-        user_is_participant = await sync_to_async(
-            utils.check_user_is_a_participant_of_chat
-        )(user, chat_id)
-        if not user_is_participant:
-            await self.disconnect()
+        # Add user to all related private chats (use sync_to_async)
+        private_chats = await sync_to_async(list)(user.private_chats.all())
+        for private_chat in private_chats:
+            await self.channel_layer.group_add(
+                str(private_chat.pk),
+                self.channel_name,
+            )
 
-        self.chat_id = chat_id
-
+        # Add user to their private channel
         await self.channel_layer.group_add(
-            self.chat_id,
+            str(user.pk),
             self.channel_name,
         )
 
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.chat_id, self.channel_name)
+        for group in self.private_chats_list:
+            await self.channel_layer.group_discard(
+                group.pk,
+                self.channel_name,
+            )
 
-    async def receive(self, text_data):
-        text_data_json = ujson.loads(text_data)
-        body = text_data_json["body"]
-        sender = self.scope["user"]
+    async def receive(self, text_data=None, bytes_data=None):
+        received_data = ujson.loads(text_data)
+        type = received_data.get("type", None)
 
-        message = await sync_to_async(models.PrivateTextMessage.objects.create)(
-            sender=sender,
-            related_chat_id=self.chat_id,
-            body=body,
-        )
+        match type:
+            case schemas.MessageType.PRIVATE_TEXT_MESSAGE.value:
+                await self.handle_private_text_message(received_data)
+            case _:
+                await self.raise_error("Unsupported message type or invalid request.")
 
+    async def raise_error(self, detail):
         await self.channel_layer.group_send(
-            self.chat_id,
+            str(self.scope["user"].pk),
             {
-                "type": "chat_message",
-                "body": message.body,
-                "modified_at": str(message.modified_at.isoformat()),
-                "sender": sender.username,
+                "type": "error",
+                "detail": detail,
             },
         )
 
-    async def chat_message(self, event):
-        body = event["body"]
-        sender = event["sender"]
-        modified_at = event["modified_at"]
+    async def error(self, event):
+        await self.send(text_data=ujson.dumps(event))
 
-        await self.send(
-            text_data=ujson.dumps(
-                {
-                    "type": "text_message",
-                    "body": f"{body}",
-                    "sender": f"{sender}",
-                    "modified_at": f"{modified_at}",
-                }
+    async def handle_private_text_message(self, received_data: dict):
+        try:
+            data = schemas.PrivateTextMessageSchema(**received_data)
+            sender = self.scope["user"]
+            receiver = await sync_to_async(models.User.objects.filter)(
+                username=data.receiver
             )
-        )
+            receiver_exists = await sync_to_async(receiver.exists)()
+
+            if not receiver_exists:
+                await self.raise_error(f"No user found with username {data.receiver}.")
+                return
+
+            receiver = await sync_to_async(receiver.get)(username=data.receiver)
+
+            if sender.username == receiver.username:
+                await self.raise_error("Cannot send message to yourself.")
+                return
+
+            private_chat = await sync_to_async(utils.get_or_create_private_chat)(
+                [sender, receiver]
+            )
+
+            message = await sync_to_async(models.PrivateTextMessage.objects.create)(
+                sender=sender,
+                related_chat_id=private_chat.pk,
+                body=data.body,
+            )
+
+            await self.channel_layer.group_send(
+                str(private_chat.pk),
+                {
+                    "type": schemas.MessageType.PRIVATE_TEXT_MESSAGE.value,
+                    "body": message.body,
+                    "sender": sender.username,
+                    "receiver": receiver.username,
+                    "modified_at": message.modified_at.strftime(DATETIME_FORMAT),
+                },
+            )
+
+        except PydanticValidationError:
+            await self.raise_error(f"Not a valid schema for type `{type}`.")
+
+    async def private_text_message(self, event):
+        await self.send(text_data=ujson.dumps(event))
 
 
 class ChannelConsumer(AsyncWebsocketConsumer):
--- core/chat/routing.py
+++ core/chat/routing.py
@@ -1,16 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 
 from core.chat import consumers
 
 ws_urlpatterns = [
-    re_path(
-        r"^ws/chat/(?P<id>[0-9a-f-]+)/$",
-        consumers.ChatConsumer.as_asgi(),
-        name="chat",
-    ),
     path(
-        "ws/channel/",
-        consumers.ChannelConsumer.as_asgi(),
-        name="channel",
+        "ws/socket/",
+        consumers.WSConsumer.as_asgi(),
+        name="socket",
     ),
 ]
